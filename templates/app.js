eefc6b06a6fc19f42c8a11ad48312e5b99736b74
santosgagbegnon <sgagb074@uottawa.ca>
2019-03-31 17:32:33 -0400

    fixed layout on sidebar, scaling image while keeping ratio

--- templates/app.js
+++ templates/app.js
@@ -13,7 +13,45 @@ const next_butotn = document.getElementById("next-button")
 const previous_button = document.getElementById("previous-button")
 
 //Stage where image and shapes will be drawn
+console.log(container_div.offsetHeight)
+
 const stage = new Konva.Stage({
-    container: "container"
+    container: "container",
+    width: container_div.offsetWidth,
+    height: container_div.offsetHeight,
 })
 
+//layer that only contains the image
+const imageLayer = new Konva.Layer()
+
+const imageObject = new Image()
+imageObject.src = "assets/default.jpg"
+imageObject.onload = imageLoaded
+
+function imageLoaded(){
+    console.log("image loaded")
+    console.log(stage.getAttr("height")/2)
+    const image = new Konva.Image({
+        image: imageObject,
+        x: stage.width()/2,
+        y: 0,
+        width: imageObject.width,
+        height: imageObject.height,
+    })
+    image.offsetX(imageObject.width/2)
+    const scale = scaleSize(stage, imageObject)
+    console.log(scale)
+    image.scale({
+        x: scale,
+        y: scale
+    })
+    imageLayer.add(image)
+    stage.add(imageLayer)
+}
+
+function scaleSize(stage, imageObject){
+    let widthRatio = stage.getAttr("width") / imageObject.width
+    let heightRatio = stage.getAttr("height") / imageObject.height
+    console.log(widthRatio)
+    return Math.min(widthRatio,heightRatio)
+}
